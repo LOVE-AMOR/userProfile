89f8b7ba82ad98f75f37aae9216e8f81a87dbd2d
LOVE-AMOR <loveamorbalaqui2@gmail.com>
2024-11-04 01:56:25 -0800

    userProfile

---
+++ userProfile.js
@@ -0,0 +1,9 @@
+let fullname = "Balaqui Love Amor";
+let age = 19;
+let favoriteNumber = 16;
+let favoriteColor = "Neon Green";
+
+console.log("Full Name:", fullname);
+console.log("Age:", age);
+console.log("Favorite Number:", favoriteNumber);
+console.log("Favorite Color:", favoriteColor)
